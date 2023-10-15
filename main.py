22846d3d804ca45d990720748dec8a2ccbd79923
dev-lovi <santiagolovisotto@gmail.com>
2023-10-15 19:40:20 -0300

    Day 67

--- main.py
+++ main.py
@@ -3,16 +3,17 @@ from kivymd.app import MDApp
 from kivy.uix.screenmanager import ScreenManager
 from kivy.lang import Builder
 
+
 class Ui(ScreenManager):
     pass
-    #def build(self): Day 66 freeday
+    #def build(self): Day 67 
         # Setting theme to my favorite theme
         #self.theme_cls.primary_palette = "DeepPurple"
         #self.audio_files = ["audio1.mp3", "audio2.mp3", "audio3.mp3"]
     
 class MainApp(MDApp):
     def build(self):
-        self.theme_cls.theme_style = "Light"
+        self.theme_cls.theme_style = "Dark"
         self.theme_cls.primary_palette = "DeepPurple"
         Builder.load_file("main.kv")
 
