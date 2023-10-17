ce0b4adba4db6704734849fe45381cb9da8b9fcb
dev-lovi <santiagolovisotto@gmail.com>
2023-10-17 18:21:13 -0300

    Day 69

--- main.py
+++ main.py
@@ -1,29 +1,17 @@
-#main.py
-from kivymd.app import MDApp
-from kivy.uix.screenmanager import ScreenManager
+#Starting from zero
+from kivy.uix.widget import Widget
 from kivy.lang import Builder
-from kivy.app import App
-from kivy.uix.gridlayout import GridLayout
+from kivymd.app import MDApp
 
-class MainWidget(GridLayout): 
+# Designate your kv file
+Builder.load_file('main.kv')
+
+class MyLayout(Widget):
     pass
 
-
-#class Ui(ScreenManager):
- #   pass
-    #def build(self): Day 67 
-        # Setting theme to my favorite theme
-        #self.theme_cls.primary_palette = "DeepPurple"
-        #self.audio_files = ["audio1.mp3", "audio2.mp3", "audio3.mp3"]
-    
-class MainApp(MDApp):
+class MyApp(MDApp):
     def build(self):
-        self.theme_cls.theme_style = "Dark"
-        self.theme_cls.primary_palette = "DeepPurple"
-        Builder.load_file("styles.kv")
-
-        #return Ui()
-        return MainWidget()
-
+        return MyLayout()
+    
 if __name__ == '__main__':
-    MainApp().run()
+    MyApp().run()
