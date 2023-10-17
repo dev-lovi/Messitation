8026e2736393407e70a346d173a5a4d9c0143841
dev-lovi <santiagolovisotto@gmail.com>
2023-10-16 21:48:10 -0300

    Day 68

--- main.py
+++ main.py
@@ -2,10 +2,15 @@
 from kivymd.app import MDApp
 from kivy.uix.screenmanager import ScreenManager
 from kivy.lang import Builder
+from kivy.app import App
+from kivy.uix.gridlayout import GridLayout
 
-
-class Ui(ScreenManager):
+class MainWidget(GridLayout): 
     pass
+
+
+#class Ui(ScreenManager):
+ #   pass
     #def build(self): Day 67 
         # Setting theme to my favorite theme
         #self.theme_cls.primary_palette = "DeepPurple"
@@ -15,9 +20,10 @@ class MainApp(MDApp):
     def build(self):
         self.theme_cls.theme_style = "Dark"
         self.theme_cls.primary_palette = "DeepPurple"
-        Builder.load_file("main.kv")
+        Builder.load_file("styles.kv")
 
-        return Ui()
+        #return Ui()
+        return MainWidget()
 
 if __name__ == '__main__':
     MainApp().run()
